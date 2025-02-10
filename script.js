3b4edc800d981411ac40a0752253db298e0a91da
Amit <81560269+immeas@users.noreply.github.com>
2025-02-10 10:11:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const student = {
+	name:"kwst",
+	age:"25",
+	city:"Daman",
+}
+
+function getkeys(obj) {
+   return student.keys(obj);
+}
+
+console.log(getkeys(student));
+	
+}
