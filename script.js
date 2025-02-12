1ecb2dd52aab8838f9a78e88975cd9a53b4a04c5
Amit <81560269+immeas@users.noreply.github.com>
2025-02-12 10:54:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 //your JS code here. If required.
 const student = {
-	name:"kwst",
-	age:"25",
-	city:"Daman",
-}
+	name: "kwst",
+	age: "25",
+	city: "Daman",
+};
 
 function getkeys(obj) {
-   return Object.keys(obj);
+   return Object.keys(obj); // ✅ Corrected method
 }
 
 console.log(getkeys(student));
-	
-}
